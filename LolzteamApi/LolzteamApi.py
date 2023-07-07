80de6f961a89c4b51cb9c086fb64dd77948dceeb
AS7RIDENIED <69326004+AS7RIDENIED@users.noreply.github.com>
2023-07-07 18:59:02 +0300

    1.0.11 Welcome to pypi

--- LolzteamApi/LolzteamApi.py
+++ LolzteamApi/LolzteamApi.py
@@ -2,7 +2,7 @@ import requests
 import time
 import json
 
-
+# Lolzteam
 class LolzteamApi:
     def __init__(self, token: str, bypass_429: bool = True):
         """
---
+++ LolzteamApi/__init__.py
@@ -0,0 +1,1 @@
+from .LolzteamApi import LolzteamApi
